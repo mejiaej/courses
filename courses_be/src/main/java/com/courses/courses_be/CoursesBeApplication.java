88e5c26289ada90c698c6550254a62246c889e8d
mejiaej <mejiaej@gmail.com>
2025-03-22 10:22:34 -0500

    chore: project setup

---
+++ courses_be/src/main/java/com/courses/courses_be/CoursesBeApplication.java
@@ -0,0 +1,13 @@
+package com.courses.courses_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoursesBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoursesBeApplication.class, args);
+	}
+
+}
---
+++ courses_be/src/test/java/com/courses/courses_be/CoursesBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.courses.courses_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
