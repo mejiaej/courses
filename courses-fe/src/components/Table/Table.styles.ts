1d6d6463055ea70b9834d13532705765f9220aef
mejiaej <mejiaej@gmail.com>
2025-03-23 14:38:48 -0500

    feat: add filter

---
+++ courses-fe/src/components/Table/Table.styles.ts
@@ -0,0 +1,7 @@
+import styled from "@emotion/styled";
+import { Box } from "@mui/material";
+
+export const StyledSearchContainer = styled(Box)`
+  display: flex;
+  margin-bottom: 1rem;
+`;
--- courses-fe/src/components/Table/Table.tsx
+++ courses-fe/src/components/Table/Table.tsx
@@ -1,26 +1,46 @@
-import { Paper, Table as MuiTable, TableContainer } from "@mui/material";
-import { useReactTable, getCoreRowModel } from "@tanstack/react-table";
+import { Paper, Table as MuiTable, TableContainer, TextField, Box } from "@mui/material";
+import { useReactTable, getCoreRowModel, getFilteredRowModel } from "@tanstack/react-table";
 import { TableProps } from "./Table.types";
 import TableBody from "./TableBody";
 import TableHeader from "./TableHeader";
+import { useState } from "react";
+import { StyledSearchContainer } from "./Table.styles";
 
 const Table = (props: TableProps) => {
   const { data, columns } = props;
+
+  const [globalFilter, setGlobalFilter] = useState("");
 
   const table = useReactTable({
     data: data || [],
     columns,
     getCoreRowModel: getCoreRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
     getRowId: (row: object) => (row as { id: string })?.id,
+    state: {
+      globalFilter,
+    },
+    onGlobalFilterChange: setGlobalFilter,
   });
 
   return (
-    <TableContainer component={Paper}>
-      <MuiTable aria-label="table component">
-        <TableHeader headers={table?.getHeaderGroups()} />
-        <TableBody rowModel={table?.getRowModel()} />
-      </MuiTable>
-    </TableContainer>
+    <>
+      <StyledSearchContainer>
+        <TextField
+          fullWidth
+          label="Search"
+          variant="outlined"
+          value={globalFilter}
+          onChange={(e) => setGlobalFilter(e.target.value)}
+        />
+      </StyledSearchContainer>
+      <TableContainer component={Paper}>
+        <MuiTable aria-label="table component">
+          <TableHeader headers={table?.getHeaderGroups()} />
+          <TableBody rowModel={table?.getRowModel()} />
+        </MuiTable>
+      </TableContainer>
+    </>
   );
 };
 
